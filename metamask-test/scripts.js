2cfba36746392df3b64567e936018d736716d033
Mithru Vigneshwara <mithru@users.noreply.github.com>
2021-12-25 02:29:26 -0500

    Add files via upload

---
+++ metamask-test/scripts.js
@@ -0,0 +1,38 @@
+
+const ethereumButton = document.querySelector('#key');
+
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  document.getElementById('key').innerHTML = account + '';
+  document.getElementById('key').style.fontSize = '1em'
+  document.getElementById('details').innerHTML = "The above is your MetaMask wallet's public key. It is safe to share this key with people or services you wish to interact with. ";
+}
+
+
+if (typeof window.ethereum !== 'undefined') {
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = "red";
+
+  console.log(ethereum.isMetaMask)
+  console.log(ethereum.networkVersion)
+  // ethereum.request({ method: 'eth_requestAccounts' });
+  console.log(ethereum.selectedAddress)
+
+  if(ethereum.selectedAddress === null){
+    ethereumButton.addEventListener('click', () => {
+      getAccount();
+    });
+  }
+
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = "green";
+
+  document.getElementById('mainText').innerHTML = "This page tests if your browser has metamask installed and running. Looks like a metamask wallet is already connected. ✅"
+  document.getElementById('key').innerHTML = "Click <u>here</u> to connect to your Metamask Wallet and reveal your public key."
+
+} else {
+  console.log("ethereum is not defined");
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = "maroon";
+  document.getElementById('mainText').innerHTML = "This page tests if your browser has metamask installed and running. At the moment, the page did not detect any metamask wallet connected. ❌ <br> <br> The easiest way to use Metamask is by opening the Chrome desktop web browser and <a href='https://chrome.google.com/webstore/detail/nkbihfbeogaeaoehlefnkodbefgpgknn'> downloading the MetaMask Extension by clicking this link</a>."
+
+}
